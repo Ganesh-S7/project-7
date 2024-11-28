f10552f59ec529af4019021b298d18f0b2a93a57
Ganesh S <ganeshsganeshs20@gmail.com>
2024-11-28 19:37:31 +0530

    updated search

--- src/index.js
+++ src/index.js
@@ -9,18 +9,13 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 const redirectUri = window.location.hostname === 'localhost'
   ? 'http://localhost:3000/project-7/'  
   : 'https://ganesh-s7.github.io/project-7/';  
-
-const returnToUrl = window.location.hostname === 'localhost'
-  ? 'http://localhost:3000/project-7/'  
-  : 'https://ganesh-s7.github.io/project-7/';  
-
+ 
 root.render(
   <Auth0Provider
     domain="dev-6zg6jdsacy4xey1s.us.auth0.com"
     clientId="Rzf9y57WdahDUq3vOnqhHC9Yb99L9vgb"
     authorizationParams={{
-      redirect_uri: redirectUri,  
-      returnTo: returnToUrl,  
+      redirect_uri: redirectUri 
     }}    
   >
     <App />
